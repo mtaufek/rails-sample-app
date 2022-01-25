b7c74c353675a3cf66f9c5fc150793b1c6b376fe
adib thaqif <mtaufek@ucsd.edu>
2022-01-25 17:32:04 +0800

    Fixed prod bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   }
 
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net'
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
